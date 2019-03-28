cbf06bfcf76c3cd74d66247b2cd281b732643d3a
mengmeng liu <lxmshipg@gmail.com>
2019-03-28 03:43:26 -0400

    First

---
+++ app/src/main/java/com/example/writingtable/activity_wr.java
@@ -0,0 +1,15 @@
+package com.example.writingtable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class activity_wr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wr);
+
+    }
+}
